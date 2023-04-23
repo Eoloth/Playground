019bda5e8e583d16382cfe8aa46a6fbd4374a7bb
Eoloth <53416886+Eoloth@users.noreply.github.com>
2023-04-23 16:23:47 -0400

    Archivos agregados

---
+++ Caracteres, Unicodes y Emojis.playground/Pages/Concatenacion e interpolacion.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+// Concatenacion
+var myString = "Bienvenidos" + " " + "al curso de Swift"
+myString += "¿Os esta gustando la clase?"
+
+let name = "Ruben Manriquez"
+let country = "Chile"
+let myPersonalInfo = "Mi nombre es " + name + " y vivo en " + country
+
+// Inerpolacion
+let myPersonalInfoWithInterpolation = "Mi nombre es  \(name) y vivo en \(country)"
